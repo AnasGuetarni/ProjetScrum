278bc3821457e8d6409b3908ad57c76ae2b85f7a
kemar74 <marc.hartley@hotmail.com>
2016-09-24 11:17:48 +0200

    Renommage de models

--- model/Photo.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-class Hotel extends Model{
-	protected $pho_id;
-	protected $pho_url;
-	protected $avi_id;
-	protected $hot_id;
-}
---
+++ model/T_e_hotel_hot.php
@@ -0,0 +1,15 @@
+<?php 
+
+class Hotel extends Model{
+	protected $_htr_id;
+	protected $_htr_mel;
+	protected $_htr_motpasse;
+	protected $_htr_nom;
+	protected $_htr_prenom;
+	protected $_htr_adrligne1;
+	protected $_htr_adrligne2;
+	protected $_htr_cp;
+	protected $_htr_ville;
+	protected $_htr_etat;
+	protected $_pay_id;
+}
