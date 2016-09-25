30cd2140052238f5c94bf3348c52b8525641b19e
AnasGuetarni <anas.guetarni@gmail.com>
2016-09-25 19:05:01 +0200

    Changements CSS

--- view/site/login.php
+++ view/site/login.php
@@ -14,33 +14,34 @@ foreach($pays as $unPays){
 }
 $btnEnregister = "S'enregistrer";
 ?>
+<div class="divConnexion">
+	<h2 id="titreConnexion">Connexion</h2>
 
-<h2>Connexion</h2>
-
-<form action='?r=site/connexion' method='POST' id='connexionForm' onSubmit='return verifForm(this)'>
-<?php 
-	foreach($parametres as $data)
-	{
-		$verif = "";
-		if(isset($data[3])){
-			switch($data[3]){
-				case 'req' : $verif = " onBlur='verifChamps(this)' "; break;
-				case 'mail' : $verif = " onBlur='verifMail(this)' "; break;
-				case 'mdp' : $verif = " onBlur='verifMDP(this)' "; break;
-				//case 'remdp' : $verif = " onBlur='verifREMDP(this)' "; break;
-				case 'cp' : $verif = " onBlur='verifCP(this)' "; break;
+	<form action='?r=site/connexion' method='POST' id='connexionForm' onSubmit='return verifForm(this)'>
+	<?php 
+		foreach($parametres as $data)
+		{
+			$verif = "";
+			if(isset($data[3])){
+				switch($data[3]){
+					case 'req' : $verif = " onBlur='verifChamps(this)' "; break;
+					case 'mail' : $verif = " onBlur='verifMail(this)' "; break;
+					case 'mdp' : $verif = " onBlur='verifMDP(this)' "; break;
+					//case 'remdp' : $verif = " onBlur='verifREMDP(this)' "; break;
+					case 'cp' : $verif = " onBlur='verifCP(this)' "; break;
+				}
+			}
+			
+			if($data[2] == 'input')
+			{
+				echo "<p><label for='".$data[0]."'>".$data[1]." : </label><input id='".$data[0]."' type='text' name='".$data[0]."' ". $verif ." onFocus='noProblemo(this)'/></p>";
+			}
+			else if($data[2] == 'pass')
+			{
+				echo "<p><label for='".$data[0]."'>".$data[1]." : </label><input id='".$data[0]."' type='password' name='".$data[0]."' ". $verif ." onFocus='noProblemo(this)'/></p>";
 			}
 		}
-		
-		if($data[2] == 'input')
-		{
-			echo "<p><label for='".$data[0]."'>".$data[1]." : </label><input id='".$data[0]."' type='text' name='".$data[0]."' ". $verif ." onFocus='noProblemo(this)'/></p>";
-		}
-		else if($data[2] == 'pass')
-		{
-			echo "<p><label for='".$data[0]."'>".$data[1]." : </label><input id='".$data[0]."' type='password' name='".$data[0]."' ". $verif ." onFocus='noProblemo(this)'/></p>";
-		}
-	}
-?>
-	<input type='submit' id='registerHotelier' value="<?php print htmlspecialchars($btnEnregister);?>"/>
-</form>
+	?>
+		<input type='submit' id='registerHotelier' value="<?php print htmlspecialchars($btnEnregister);?>"/>
+	</form>
+</div>
