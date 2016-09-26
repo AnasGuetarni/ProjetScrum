7f2dd9d2d2774447918d17fd79361cb83b0016a5
webstersr <mickael.koc@etu.univ-smb.fr>
2016-09-26 09:32:54 +0200

    Changements register

--- view/hotelier/register.php
+++ view/hotelier/register.php
@@ -1,61 +1,24 @@
 <?php 
-echo "<script src='verifications.js'></script>";
 
-$parametres = Array(
-			Array('htr_nom' , 'Nom', 'input', 'req'),
-			Array('htr_prenom' , 'Prenom', 'input', 'req'),
-			Array('htr_mel' , 'E-mail', 'input', 'mail'),
-			Array('htr_motpasse' , 'Entrez un mot de passe', 'pass', 'mdp'),
-			Array('no_mdp' , 'Retapez votre mot de passe', 'pass', 'remdp'),
-			Array('htr_adrligne1' , 'Adresse', 'input', 'req'),
-			Array('htr_adrligne2' , 'Complement d\'adresse', 'input'),
-			Array('htr_cp' , 'Code postal', 'input', 'cp'),
-			Array('htr_ville' , 'Ville', 'input', 'req'),
-			Array('htr_etat' , 'Etat', 'input'),
-			Array('pay_id', 'Pays', 'select')
-				);
-
-$pays = Pays::findAll();
-
-$options_pay_id = Array();
-foreach($pays as $unPays){
-	$options_pay_id[$unPays->pay_id] = $unPays->pay_nom;
-}
+$parametres = Array('htr_nom' => 'Nom',
+				'htr_prenom' => 'Prenom',
+				'htr_mel' => 'E-mail',
+				'htr_motpasse' => 'Entrez un mot de passe',
+				'no_mdp' => 'Retapez votre mot de passe',
+				'htr_adrligne1' => 'Adresse',
+				'htr_adrligne2' => 'Complement d\'adresse',
+				'htr_cp' => 'Code postal',
+				'htr_ville' => 'Ville',
+				'htr_etat' => 'Etat');
 $btnEnregister = "S'enregistrer";
 ?>
-<h2 id="inscriptionHotelier">Inscription d'hotelier</h2>
+<h2>Inscription d'hotelier</h2>
 
-<form action='?r=hotelier/add' method='POST' id='registerHotelierForm' onSubmit='return verifForm(this)'>
+<form action='' method='POST' id='registerHotelierForm'>
 <?php 
-	foreach($parametres as $data)
+	foreach($parametres as $data=>$text)
 	{
-		$verif = "";
-		if(isset($data[3])){
-			switch($data[3]){
-				case 'req' : $verif = " onBlur='verifChamps(this)' "; break;
-				case 'mail' : $verif = " onBlur='verifMail(this)' "; break;
-				case 'mdp' : $verif = " onBlur='verifMDP(this)' "; break;
-				//case 'remdp' : $verif = " onBlur='verifREMDP(this)' "; break;
-				case 'cp' : $verif = " onBlur='verifCP(this)' "; break;
-			}
-		}
-		
-		if($data[2] == 'input')
-		{
-			echo "<p><label for='".$data[0]."'>".$data[1]." : </label><input id='".$data[0]."' type='text' name='".$data[0]."' ". $verif ." onFocus='noProblemo(this)'/></p>";
-		}
-		else if($data[2] == 'pass')
-		{
-			echo "<p><label for='".$data[0]."'>".$data[1]." : </label><input id='".$data[0]."' type='password' name='".$data[0]."' ". $verif ." onFocus='noProblemo(this)'/></p>";
-		}
-		else if($data[2] == 'select'){
-			echo "<p><label for='".$data[0]."'>".$data[1]." : </label><select id='".$data[0]."' name='".$data[0]."'>";
-			$liste = $options_pay_id;
-			foreach($liste as $id=>$pays){
-				echo "<option value=".$id.">$pays</option>";
-			}
-			echo "</select></p>";
-		}
+		echo "<p><label for='$data'>$text : </label><input id='$data' type='text' name='$data'/></p>";
 	}
 ?>
 	<input type='submit' id='registerHotelier' value="<?php print htmlspecialchars($btnEnregister);?>"/>
