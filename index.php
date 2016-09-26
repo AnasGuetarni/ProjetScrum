b9182ef8e7941573a5f3eac2686f222c556ac537
kemar74 <marc.hartley@hotmail.com>
2016-09-26 08:38:29 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,2 +1,14 @@
-<?php 
-include_once "view/hotel/index.php";
+<?php
+
+// Debug. Désactiver en prod
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors', true);
+
+// Rien de plus !!
+include_once "db.php";
+include_once "tools.php";
+
+session_start(); 
+include_once "controller/route.php";
+
+
