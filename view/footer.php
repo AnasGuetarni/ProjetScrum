adcef4af101e8823e560af9826775bdfd715de52
AnasGuetarni <anas.guetarni@gmail.com>
2016-09-26 08:41:00 +0200

    Modification header and footer

--- view/footer.php
+++ view/footer.php
@@ -1,9 +1,18 @@
 
 </section>
 
-<footer>
-Copyleft 2016
-</footer>
+	<footer id="footer">
+		<div class="inner">
+			<h2>&copy; Le site le plus swagg 2016</h2>
+			<p>Designed By Ananas, Marco, Mr Tomasi, Julos et le sheitan.</p>
+			<ul class="icons">
+				<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+				<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+				<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+				<li><a href="#" class="icon fa-envelope"><span class="label">Email</span></a></li>
+			</ul>
+		</div>
+	</footer>
 
 </main>
 
--- view/header.php
+++ view/header.php
@@ -1,28 +1,35 @@
 <!DOCTYPE html> 
 <html>
-<head>
-	<meta charset="UTF-8">
-	<title>Trip Advisor</title>
-	<link rel="stylesheet" type="text/css" href="./css/style.css" />
-</head>
-<body>
-	<main>
-	<header>
-		<h1><a href='.'>TripAdvisor</a></h1>
-	</header>
-	<nav>
-		<ul>
-			<?php if((!isset($_SESSION['id_hotelier']) || $_SESSION['id_hotelier'] == '') && (!isset($_SESSION['id_abonne']) || $_SESSION['id_abonne'] == '')){?>
-				<li><a href="?r=hotelier/register">S'inscrire en tant qu'hotelier</a></li>
-				<li><a href="?r=site/login">Se connecter</a></li>
-			<?php } else if(isset($_SESSION['id_hotelier']) && $_SESSION['id_hotelier'] != '') { ?>
-				<li><a href="?r=photo/addPhoto">Ajouter une photo</a></li>
-				<li><a href="?r=hotel/add">Créer un hotel</a></li>
-				<li><a href="?r=hotel/set">Modifier un hotel</a></li>
-			<?php } if(!(!isset($_SESSION['id_hotelier']) || $_SESSION['id_hotelier'] == '') || !(!isset($_SESSION['id_abonne']) || $_SESSION['id_abonne'] == '')){?>
-			<li><a href="?r=site/logout">Deconnexion</a></li>
-			<li>Connecté en tant que <?php echo Hotelier::findById($_SESSION['id_hotelier'])->htr_nom . "(hotelier)"; ?> </li>
-			<?php } ?>
-		</ul>
-	</nav>
-	<section>	
+	<head>
+		<meta charset="UTF-8">
+		<title>Trip Advisor</title>
+		<!--<link rel="stylesheet" type="text/css" href="./css/style.css" /><-->
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="view/site/assets/css/main.css" />
+	</head>
+
+	<body id="top">
+		<main>
+			<section id="banner" data-video="view/site/images/banner">
+				<div class="inner">
+					<header>
+						<h1><a href='.'>TripAdvisor</a></h1>
+					</header>
+					<a href="#main" class="more">Learn More</a>
+				</div>
+			</section>
+		<nav id="navigation">
+			<ul>
+				<?php if((!isset($_SESSION['id_hotelier']) || $_SESSION['id_hotelier'] == '') && (!isset($_SESSION['id_abonne']) || $_SESSION['id_abonne'] == '')){?>
+					<li><a href="?r=hotelier/register">S'inscrire en tant qu'hotelier</a></li>
+					<li><a href="?r=site/login">Se connecter</a></li>
+				<?php } else if(isset($_SESSION['id_hotelier']) && $_SESSION['id_hotelier'] != '') { ?>
+					<li><a href="?r=photo/addPhoto">Ajouter une photo</a></li>
+					<li><a href="?r=hotel/add">Créer un hotel</a></li>
+					<li><a href="?r=hotel/set">Modifier un hotel</a></li>
+				<?php } if(!(!isset($_SESSION['id_hotelier']) || $_SESSION['id_hotelier'] == '') || !(!isset($_SESSION['id_abonne']) || $_SESSION['id_abonne'] == '')){?>
+				<li><a href="?r=site/logout">Deconnexion</a></li>
+				<?php } ?>
+			</ul>
+		</nav>
