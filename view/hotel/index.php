3450a7c2cfe8bd459817b9104f41b1dc43e4ac37
kemar74 <marc.hartley@hotmail.com>
2016-09-26 09:12:10 +0200

    index changé

--- view/hotel/index.php
+++ view/hotel/index.php
@@ -4,10 +4,10 @@ $hotels = Hotel::findAll();
 ?>
 <h2>Index des Hotels</h2>
 
-<ul>
+<div class="thumbnails">
 <?php 
 foreach($hotels as $hotel){
-	echo "<li><a href='?r=hotel/view&id=".$hotel->hot_id."'>".$hotel->hot_nom."</a></li>";
+	echo "<div class='box'><div class='inner'><a href='?r=hotel/view&id=".$hotel->hot_id."'><img src='".(count($hotel->photos)>0?$hotel->photos[0]->pho_url:' ')."' />".$hotel->hot_nom."</a></div></div>";
 }
 ?>
-</ul>
+</div>
