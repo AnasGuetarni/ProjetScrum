b78a548b6e7f08c8a2473e5892921a6d442fe66e
webstersr <mickael.koc@etu.univ-smb.fr>
2016-09-26 09:36:02 +0200

    Changements Micka

--- controller/HotelController.php
+++ controller/HotelController.php
@@ -9,7 +9,11 @@ class HotelController extends Controller {
 	
 
 	public function add() {
-		$this->render("add");
+		//$this->render("add");
+		$hotel = new t_e_hotel_hot(1,1,parameters()["prx_id"],parameters()["hot_nom"],parameters()["hot_description"],parameters()["hot_adrligne1"],parameters()["hot_adrligne2"],
+								 parameters()["hot_cp"],parameters()["hot_ville"],parameters()["hot_etat"],parameters()["pay_id"],1,1,parameters()["cat_nbetoiles"],
+								parameters()["hot_indicatif"],parameters()["hot_tel"],parameters()["hot_mel"],parameters()["hot_siteweb"],parameters()["hot_nbchambres"]);
+		
 	}
 	public function index() {
 		$this->render("index");
