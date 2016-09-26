50122554a6ab74506d2c485001e82350365d2226
kemar74 <marc.hartley@hotmail.com>
2016-09-26 08:22:33 +0200

    site/index.php

--- index.php
+++ index.php
@@ -1,14 +1,2 @@
-<?php
-
-// Debug. Désactiver en prod
-error_reporting(E_ALL | E_STRICT);
-ini_set('display_errors', true);
-
-// Rien de plus !!
-include_once "db.php";
-include_once "tools.php";
-
-session_start(); 
-include_once "controller/route.php";
-
-
+<?php 
+include_once "view/hotel/index.php";
